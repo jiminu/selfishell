70b8d0c9ecb14b799a5360e223f92cf18bd9d061
jiminu <jmu417@gmail.com>
2026-07-18 00:01:32 +0900

    fix(config): remove nvim-treesitter config to resolve startup module load issues

--- common/init.lua
+++ common/init.lua
@@ -53,6 +53,22 @@ require("lazy").setup({
       })
     end,
   },
+  -- Rainbow delimiters (VS Code Style Bracket Colorizer)
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "BufReadPost",
+    config = function()
+      local rainbow_delimiters = require('rainbow-delimiters')
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+        },
+      }
+    end,
+  },
 })
 
 -- Options
