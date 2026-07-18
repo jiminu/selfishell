04bd0ebb2e7900e508b3060dfcdd54b674bd8f00
jiminu <jmu417@gmail.com>
2026-07-18 13:00:44 +0900

    feat(config): auto-start Treesitter syntax highlighting for installed language parsers

--- common/init.lua
+++ common/init.lua
@@ -231,3 +231,13 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
+-- Enable Treesitter highlighting automatically for installed languages
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+    if lang then
+      pcall(vim.treesitter.start, args.buf, lang)
+    end
+  end,
+})
