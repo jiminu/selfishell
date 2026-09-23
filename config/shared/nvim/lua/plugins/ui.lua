55091944e5ab67dea6dad22a23e87fb53e799a14
jiminu <jmu417@gmail.com>
2026-09-24 01:16:18 +0900

    Distinguish scrollbar errors from Git deletions

--- config/shared/nvim/lua/plugins/ui.lua
+++ config/shared/nvim/lua/plugins/ui.lua
@@ -605,9 +605,9 @@ return {
         color = "#797979",
       },
       marks = {
+        Error = { text = { "•", "•" } },
         GitAdd = { text = "│" },
         GitChange = { text = "│" },
-        GitDelete = { text = "┄" },
       },
       excluded_filetypes = {
         "cmp_docs",
